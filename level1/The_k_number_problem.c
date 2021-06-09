46646ad58fa9587da59ad9b50bd399b5e460c9e8
BU <46125548+thankdesert@users.noreply.github.com>
2021-06-09 14:25:47 +0900

    Create The_k_number_problem.c

---
+++ level1/The_k_number_problem.c
@@ -0,0 +1,44 @@
+  
+/*
+---------------------------------------------
+The_k_number_problem
+from : https://programmers.co.kr/learn/courses/30/lessons/42748
+solved by using stack.
+---------------------------------------------
+*/
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void init_arr(vector<int> array, int* arr) {
+    for(int i = 0; i<array.size(); i++)
+        arr[i] = array[i];
+}
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    int* arr;
+    int left, right, rank, temp;
+    arr = (int*)malloc(sizeof(int)*array.size());
+    
+    for(int i = 0; i<commands.size(); i++) {
+        left = commands[i][0];
+        right = commands[i][1];
+        rank = commands[i][2];
+        init_arr(array, arr);
+        for(int j = left - 1; j < right; j++) {
+            for(int k = j +1; k < right; k++) {
+                if(arr[j] > arr[k]) {
+                    temp = arr[j];
+                    arr[j] = arr[k];
+                    arr[k] = temp;
+                }
+            }
+        }
+        answer.push_back(arr[left + rank - 2]);
+    }
+    return answer;
+}
