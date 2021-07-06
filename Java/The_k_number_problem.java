fb56806e518f1e7064ec58083020f80ba28e0c58
BU <46125548+thankdesert@users.noreply.github.com>
2021-07-06 16:18:34 +0900

    Create The_k_number_problem.java

---
+++ Java/The_k_number_problem.java
@@ -0,0 +1,21 @@
+/*
+---------------------------------------------
+The_k_number_problem
+from : https://programmers.co.kr/learn/courses/30/lessons/42748
+---------------------------------------------
+*/
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]); 
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
