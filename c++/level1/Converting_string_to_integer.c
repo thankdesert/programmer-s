3c27658de148a13fe8dbd3a5837e410589938a50
BU <46125548+thankdesert@users.noreply.github.com>
2021-07-15 13:08:32 +0900

    Create Converting_string_to_integer.c

---
+++ c++/level1/Converting_string_to_integer.c
@@ -0,0 +1,15 @@
+/*
+---------------------------------------------
+Converting_a_string_to_integer
+from : https://programmers.co.kr/learn/courses/30/lessons/12925?language=cpp
+---------------------------------------------
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
