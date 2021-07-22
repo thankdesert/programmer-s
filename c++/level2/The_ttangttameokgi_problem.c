25984fbd13f821be7d9120c718f38510d45cbded
BU <46125548+thankdesert@users.noreply.github.com>
2021-07-22 22:42:37 +0900

    Create The_ttangttameokgi_problem.c

---
+++ c++/level2/The_ttangttameokgi_problem.c
@@ -0,0 +1,22 @@
+/*
+---------------------------------------------
+The_ttangttameokgi_problem
+from : https://programmers.co.kr/learn/courses/30/lessons/12913
+---------------------------------------------
+*/
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> land) {
+    for(int i = 1; i<land.size(); i++) {
+        land[i][0] += max(land[i-1][1], max(land[i-1][2], land[i-1][3]));
+        land[i][1] += max(land[i-1][0], max(land[i-1][2], land[i-1][3]));
+        land[i][2] += max(land[i-1][0], max(land[i-1][1], land[i-1][3]));
+        land[i][3] += max(land[i-1][0], max(land[i-1][1], land[i-1][2]));
+    }
+    
+    return *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
+}
