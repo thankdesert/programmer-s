06e989c67a360533e4be0324962f065339502010
BU <46125548+thankdesert@users.noreply.github.com>
2021-07-26 19:33:43 +0900

    Create The_best_combination_problem.c

---
+++ c++/level3/The_best_combination_problem.c
@@ -0,0 +1,24 @@
+/*
+---------------------------------------------
+The_best_combination_problem
+from : https://programmers.co.kr/learn/courses/30/lessons/12938
+---------------------------------------------
+*/
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+    if(s/n == 0)
+        return {-1};
+    for(int i = 0; i<n; i++)
+        answer.push_back(s/n);
+    for(int i = 0; i<s%n; i++)
+        answer[answer.size() - 1 - i] += 1;
+    
+    return answer;
+}
